2fbdced1417e3c8bc5ddded64dae84615eb67073
Anish V <102044149+AnishVDev@users.noreply.github.com>
2024-11-17 21:55:54 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,51 @@
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/**
+
+   _____         .__       .__  ____   ____________                
+  /  _  \   ____ |__| _____|  |_\   \ /   /\______ \   _______  __ 
+ /  /_\  \ /    \|  |/  ___/  |  \   Y   /  |    |  \_/ __ \  \/ / 
+/    |    \   |  \  |\___ \|   Y  \     /   |    `   \  ___/\   /  
+\____|__  /___|  /__/____  >___|  /\___/   /_______  /\___  >\_/   
+        \/     \/        \/     \/                 \/     \/       
+ ___________ __________ 
+/_   \   _  \\______   \
+ |   /  /_\  \|       _/
+ |   \  \_/   \    |   \
+ |___|\_____  /____|_  /
+            \/       \/ 
+
+*/
+
+
+/**
+ * @file main.cpp
+ * @brief This file contains the main code for the robot's operation.
  *
- * pros upload in Integrated Terminal to upload code to VEX Brain
+ * Robot Controller:
+ *
+ * Tank Drive
+ * Left Joystick (Vertical): Controls left side of drivetrain (forward/backward).
+ * Right Joystick (Vertical): Controls right side of drivetrain (forward/backward).
+ *
+ * Arcade Drive
+ * Left Joystick (Vertical): Controls movement of the drivetrain (forward/backward).
+ * Right Joystick (Horizontal): Controls rotation of the drivetrain (left/right).
+ *
+ * Controller Settings:
+ * L1: Press once to start the intake moving forward. Press again to stop the intake (if it's already running forward).
+ * L2: Press once to start the intake moving in reverse. Press again to stop the intake (if it's already running in reverse).
+ * R1: Opens the clamp by activating the pistons.
+ * R2: Closes the clamp by deactivating the pistons.
+ *
+ * Motor Settings:
+ * Motor Ports: 1-6 (left ports: 1, 2, and 3; right ports: 4, 5, and 6)
+ * Intake Port: Port 7
+ * Pneumatics (Clamp Pistons): ADI Port A
+ * Controller: USB Ports to wirelessly connect to VEX Brain
  */
 
 #include "main.h"
